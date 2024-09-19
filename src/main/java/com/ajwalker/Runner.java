f5b0053fa6aa1743c422625d3d73c9a26c58134d
EmineKarabolat <emine.karabolat.01@gmail.com>
2024-09-19 15:11:31 +0300

    first commit

--- src/main/java/com/ajwalker/Runner.java
+++ src/main/java/com/ajwalker/Runner.java
@@ -7,5 +7,7 @@ import jakarta.persistence.Persistence;
 public class Runner {
     public static void main(String[] args) {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Futbol_ManagerV2");
+        
+        System.out.println("Merhaba Dünya");
     }
 }
